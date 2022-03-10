8a87bb862d74682eed69c52ab8d5a46e5637a68f
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2022-03-10 21:03:33 +0300

    add sidebar

---
+++ index.js
@@ -0,0 +1,13 @@
+const sidebarItem = document.querySelector(".sidebar__item");
+const sidebarCloseButtons = document.querySelectorAll(
+  ".sidebar__button-close-element"
+);
+
+sidebarCloseButtons.forEach(function (item) {
+  item.addEventListener("click", function (evt) {
+    const li = evt.target.closest(".sidebar__item");
+    const sidebarListContent = li.querySelector(".sidebar__list-content");
+    sidebarListContent.classList.toggle("sidebar__list-content_opened");
+    evt.target.classList.toggle("sidebar__button-close-element_opened");
+  });
+});
